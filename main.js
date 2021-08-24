59ac6b6da2e0b1826e9023bc3e63cbdbb0d9513c
yunsuu <ljsql934@gmail.com>
2021-08-25 00:00:57 +0900

    d

--- main.js
+++ main.js
@@ -4,6 +4,28 @@ const port = 3000;
 
 const oembed = require('oembed-parser');
 
+app.set('view engine', 'ejs'); // 1
+app.use(express.static(__dirname + '/public'));
+
+app.get('/main', function (req, res) {
+  res.render('main', { name: '앙' });
+});
+
+app.get('/search', function (req, res) {
+  const url = req.query.url;
+  console.log(url);
+  oembed
+    .extract(url)
+    .then((oembed) => {
+      console.log(oembed);
+      res.render('search', { obj: oembed });
+    })
+    .catch((err) => {
+      console.trace(err);
+    });
+  // res.render('search', { name: req.params.nameParam });
+});
+
 app.get('/', (req, res) => {
   //   const url = 'https://www.youtube.com/watch?v=dBD54EZIrZo';
   //   const url = 'https://www.instagram.com/p/BUawPlPF_Rx/'
